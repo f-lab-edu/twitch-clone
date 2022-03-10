334f1eacab01caa2f9a1178fd4e08459101316d5
hyune <choi8608@gmail.com>
2022-03-10 23:27:44 +0900

    #5 project init

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
