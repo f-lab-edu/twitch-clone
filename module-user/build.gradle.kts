07e62a518345f1b498e39200e9a3e7ddeed37998
DESKTOP-T6O4081\lee <lsecret@naver.com>
2022-03-20 15:36:56 +0900

    test

--- module-user/build.gradle.kts
+++ module-user/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.6.10"
+    id("io.gitlab.arturbosch.detekt").version("1.19.0")
 }
 
 group = "com.example"
@@ -8,6 +9,7 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
 }
 
 dependencies {
@@ -16,4 +18,14 @@ dependencies {
     // TODO 추후 버전 및 공통 dependencies 분리
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation("org.assertj:assertj-core:3.21.0")
+
+    // detekt
+    runtimeOnly("io.gitlab.arturbosch.detekt:detekt-api:1.19.0")
+}
+
+afterEvaluate {
+    detekt {
+        buildUponDefaultConfig = true
+        config.setFrom(files("$rootDir/detekt-config.yml"))
+    }
 }
