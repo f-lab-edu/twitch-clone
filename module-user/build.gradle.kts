3fb664e6caf8e1331316b64cd91f5abbfa9c71d3
DESKTOP-T6O4081\lee <lsecret@naver.com>
2022-03-22 20:20:28 +0900

    #8 detekt lib 추가

--- module-user/build.gradle.kts
+++ module-user/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.6.10"
+    id("io.gitlab.arturbosch.detekt").version("1.19.0")
 }
 
 group = "com.example"
@@ -8,6 +9,7 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
 }
 
 dependencies {
@@ -17,4 +19,13 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation("org.assertj:assertj-core:3.21.0")
     testImplementation("com.github.javafaker:javafaker:1.0.2")
+
+    runtimeOnly("io.gitlab.arturbosch.detekt:detekt-api:1.19.0")
+}
+
+afterEvaluate {
+    detekt {
+        buildUponDefaultConfig = true
+        config.setFrom(files("$rootDir/detekt-config.yml"))
+    }
 }
