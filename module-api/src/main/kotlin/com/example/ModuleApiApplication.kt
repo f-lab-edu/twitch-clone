ccf14247dec53acaca37cbe2df01583523130b74
hyune <choi8608@gmail.com>
2022-04-15 21:11:58 +0900

    detekt 설정을 root 프로젝트 레벨로 올림

--- module-api/src/main/kotlin/com/example/ModuleApiApplication.kt
+++ module-api/src/main/kotlin/com/example/ModuleApiApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class ModuleApiApplication
 
 fun main(args: Array<String>) {
+    @Suppress("SpreadOperator")
     runApplication<ModuleApiApplication>(*args)
 }
