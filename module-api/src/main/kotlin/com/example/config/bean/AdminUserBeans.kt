e068110cd5d0c3d6aff2e42a6a94f7df9e25b8e6
hyune <choi8608@gmail.com>
2022-05-16 22:47:17 +0900

    #46 bean config 정상화

--- module-api/src/main/kotlin/com/example/config/bean/AdminUserBeans.kt
+++ module-api/src/main/kotlin/com/example/config/bean/AdminUserBeans.kt
@@ -1,13 +1,18 @@
 package com.example.config.bean
 
+import com.example.user.adpter.out.AdminUserDao
 import com.example.user.adpter.out.AdminUserRepositoryImpl
+import com.example.user.adpter.out.NormalUserRepositoryImpl
 import com.example.user.application.port.`in`.admin.CreateAdminUserUseCase
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-internal class AdminUserBeans {
+internal class AdminUserBeans(
+    private val adminUserDao: AdminUserDao,
+    private val normalUserRepositoryImpl: NormalUserRepositoryImpl,
+) {
 
     @Bean
-    fun createAdminUserUseCase(): CreateAdminUserUseCase = CreateAdminUserUseCase.create(AdminUserRepositoryImpl())
+    fun createAdminUserUseCase(): CreateAdminUserUseCase = CreateAdminUserUseCase.create(AdminUserRepositoryImpl(adminUserDao, normalUserRepositoryImpl))
 }
--- module-api/src/main/kotlin/com/example/config/bean/StreamerUserBeans.kt
+++ module-api/src/main/kotlin/com/example/config/bean/StreamerUserBeans.kt
@@ -1,7 +1,11 @@
 package com.example.config.bean
 
+import com.example.user.adpter.out.NormalUserDao
+import com.example.user.adpter.out.NormalUserDynamicDao
 import com.example.user.adpter.out.NormalUserRepositoryImpl
 import com.example.user.adpter.out.PaymentPortImpl
+import com.example.user.adpter.out.StreamerUserDao
+import com.example.user.adpter.out.StreamerUserDynamicDao
 import com.example.user.adpter.out.StreamerUserRepositoryImpl
 import com.example.user.application.port.`in`.streamer.ApproveStreamerUserUseCase
 import com.example.user.application.port.`in`.streamer.CreateStreamerUserUseCase
@@ -16,34 +20,58 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-internal class StreamerUserBeans {
+internal class StreamerUserBeans(
+    private val streamerUserDao: StreamerUserDao,
+    private val streamerUserDynamicDao: StreamerUserDynamicDao,
+    private val normalUserDao: NormalUserDao,
+    private val normalUserDynamicDao: NormalUserDynamicDao,
+    private val normalUserRepositoryImpl: NormalUserRepositoryImpl,
+) {
 
     @Bean
-    fun approveStreamerUserUseCase(): ApproveStreamerUserUseCase = ApproveStreamerUserUseCase.create(StreamerUserRepositoryImpl())
+    fun approveStreamerUserUseCase(): ApproveStreamerUserUseCase =
+        ApproveStreamerUserUseCase.create(StreamerUserRepositoryImpl(streamerUserDao, streamerUserDynamicDao, normalUserDao, normalUserRepositoryImpl))
 
     @Bean
-    fun createStreamerUserUseCase(): CreateStreamerUserUseCase = CreateStreamerUserUseCase.create(StreamerUserRepositoryImpl())
+    fun createStreamerUserUseCase(): CreateStreamerUserUseCase =
+        CreateStreamerUserUseCase.create(StreamerUserRepositoryImpl(streamerUserDao, streamerUserDynamicDao, normalUserDao, normalUserRepositoryImpl))
 
     @Bean
-    fun findPendingStreamerUserUseCase(): FindPendingStreamerUserUseCase = FindPendingStreamerUserUseCase.create(StreamerUserRepositoryImpl())
+    fun findPendingStreamerUserUseCase(): FindPendingStreamerUserUseCase =
+        FindPendingStreamerUserUseCase.create(StreamerUserRepositoryImpl(streamerUserDao, streamerUserDynamicDao, normalUserDao, normalUserRepositoryImpl))
 
     @Bean
-    fun findStreamerUserUseCase(): FindStreamerUserUseCase = FindStreamerUserUseCase.create(StreamerUserRepositoryImpl())
+    fun findStreamerUserUseCase(): FindStreamerUserUseCase =
+        FindStreamerUserUseCase.create(StreamerUserRepositoryImpl(streamerUserDao, streamerUserDynamicDao, normalUserDao, normalUserRepositoryImpl))
 
     @Bean
-    fun rejectStreamerUserUseCase(): RejectStreamerUserUseCase = RejectStreamerUserUseCase.create(StreamerUserRepositoryImpl())
+    fun rejectStreamerUserUseCase(): RejectStreamerUserUseCase =
+        RejectStreamerUserUseCase.create(StreamerUserRepositoryImpl(streamerUserDao, streamerUserDynamicDao, normalUserDao, normalUserRepositoryImpl))
 
     @Bean
     fun subscribeStreamerUserUseCase(): SubscribeStreamerUserUseCase =
-        SubscribeStreamerUserUseCase.create(NormalUserRepositoryImpl(), StreamerUserRepositoryImpl(), PaymentPortImpl())
+        SubscribeStreamerUserUseCase.create(
+            NormalUserRepositoryImpl(normalUserDao, normalUserDynamicDao),
+            StreamerUserRepositoryImpl(streamerUserDao, streamerUserDynamicDao, normalUserDao, normalUserRepositoryImpl),
+            PaymentPortImpl()
+        )
 
     @Bean
-    fun suspendStreamerUserUseCase(): SuspendStreamerUserUseCase = SuspendStreamerUserUseCase.create(StreamerUserRepositoryImpl())
+    fun suspendStreamerUserUseCase(): SuspendStreamerUserUseCase =
+        SuspendStreamerUserUseCase.create(StreamerUserRepositoryImpl(streamerUserDao, streamerUserDynamicDao, normalUserDao, normalUserRepositoryImpl))
 
     @Bean
     fun updateStreamerUserSubscriptionCostUseCase(): UpdateStreamerUserSubscriptionCostUseCase =
-        UpdateStreamerUserSubscriptionCostUseCase.create(StreamerUserRepositoryImpl())
+        UpdateStreamerUserSubscriptionCostUseCase.create(
+            StreamerUserRepositoryImpl(
+                streamerUserDao,
+                streamerUserDynamicDao,
+                normalUserDao,
+                normalUserRepositoryImpl
+            )
+        )
 
     @Bean
-    fun updateStreamerUserUseCase(): UpdateStreamerUserUseCase = UpdateStreamerUserUseCase.create(StreamerUserRepositoryImpl())
+    fun updateStreamerUserUseCase(): UpdateStreamerUserUseCase =
+        UpdateStreamerUserUseCase.create(StreamerUserRepositoryImpl(streamerUserDao, streamerUserDynamicDao, normalUserDao, normalUserRepositoryImpl))
 }
