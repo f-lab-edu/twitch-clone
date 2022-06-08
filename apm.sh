a3994b542190a8e83e4154de4f678f8d5d982db4
이정수 <jslee@dlto.co.kr>
2022-06-08 14:01:18 +0900

    #50 APM 정보 README.md 추가 및 version SNAPSHOT 제거

--- apm.sh
+++ apm.sh
@@ -4,4 +4,4 @@ java -javaagent:./elastic-apm-agent-1.31.0.jar \
 -Delastic.apm.service_name=apm-1 \
 -Delastic.apm.server_urls=http://49.50.162.164:8200 \
 -Delastic.apm.application_packages=com.example \
--jar ./module-api/build/libs/module-api-0.0.1-SNAPSHOT.jar
+-jar ./module-api/build/libs/module-api-0.0.1.jar 
--- module-api/build.gradle.kts
+++ module-api/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.example"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
